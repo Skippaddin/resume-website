2bb2b8c1d4631fd476cfff5c065bf3d08cd0f9f4
Skippaddin <masmus@uni-bremen.de>
2025-04-16 16:00:36 +0200

    Edited Techlink

--- src/components/TechLink/TechLink.jsx
+++ src/components/TechLink/TechLink.jsx
@@ -4,13 +4,13 @@ import styles from './TechLink.module.css';
 function TechLink({href, children, defaultColor = 'blue', hoverColor = 'red'}) {
   const [hovered, setHovered] = useState(false);
 
-  const styles = {
+  const inlineStyles = {
     color: hovered ? hoverColor : defaultColor,
     textDecoration: 'none'
   }
 
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer" onMouseLeave={() => setHovered(false)} onMouseEnter={() => setHovered(true)} style={styles}>{children}</a>
+    <a href={href} target="_blank" rel="noopener noreferrer" onMouseLeave={() => setHovered(false)} onMouseEnter={() => setHovered(true)} style={inlineStyles}>{children}</a>
   );
 }
 
